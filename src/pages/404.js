b0b00ca6c5aeeb6a9a75560387dc8133293b59e3
Tom Smith <me@tomm.us>
2019-11-24 12:48:44 +0000

    add 404 page. Closes #1

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,15 @@ import SEO from "../components/seo";
 
 const NotFoundPage = () => (
     <Layout>
-        <SEO title='404: Not found' />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <SEO title="Not Found" />
+        <div className="flex flex-col items-center">
+            <h1 className="font-display tracking-tight text-gray-800 leading-none text-6xl">
+                Not Found
+            </h1>
+            <p className="text-xl text-gray-500 tracking-tight text-center">
+                The page you are looking for doesn't seem to exist.
+            </p>
+        </div>
     </Layout>
 );
 
